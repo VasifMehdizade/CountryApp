b7496fa60bba982abcca9e06cb9eb762e7643c6d
VasifMehdizade <109298511+VasifMehdizade@users.noreply.github.com>
2022-11-17 23:23:37 +0400

    Onboarding controller created

---
+++ Country App/Scenes/Onboarding/OnboardingController.swift
@@ -0,0 +1,27 @@
+//
+//  OnboardingController.swift
+//  Country App
+//
+//  Created by Vasif Mehdi on 17.11.22.
+//
+
+import UIKit
+
+class OnboardingController: UIViewController {
+    @IBOutlet weak var brandLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let delay : Double = 0.5
+        DispatchQueue.main.asyncAfter(deadline: .now() + delay) {
+        fireTimer()
+            
+            
+        }
+        
+        func fireTimer() {
+            let controller = storyboard?.instantiateViewController(withIdentifier: "LoginController") as! LoginController
+            show(controller, sender: nil)
+        }
+    }
+}
