aee5fc86939ae5d6b8af9ecd939462e2989a321d
VasifMehdizade <109298511+VasifMehdizade@users.noreply.github.com>
2022-11-17 23:13:05 +0400

    ManagerFiles added

---
+++ Country App/NetworkLayer/ManagerFiles/RegisterManager.swift
@@ -0,0 +1,26 @@
+//
+//  RegisterManager.swift
+//  Country App
+//
+//  Created by Vasif Mehdi on 17.11.22.
+//
+
+import Foundation
+
+class RegisterManager {
+    static let shared = RegisterManager()
+    
+//    func getMovies (complete: @escaping ((Movies?, String?)->())) {
+//        let url = "https://api.themoviedb.org/3/movie/now_playing?api_key=d8255fdb07be4ce132643e5e155bf54b"
+//        NetworkManager.shared.request(type: Movies.self,
+//                                      url: url,
+//                                      method: .get) { response in
+//            switch response {
+//            case .success(let model) :
+//                complete(model, nil)
+//            case .failure(let error) :
+//                complete(nil, error.localizedDescription)
+//            }
+//        }
+//    }
+}
