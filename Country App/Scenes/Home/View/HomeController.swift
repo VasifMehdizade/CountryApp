035b9cb5585b3904fcdc3b7bc660005d205f6255
VasifMehdizade <109298511+VasifMehdizade@users.noreply.github.com>
2022-11-21 01:45:00 +0400

    TabBar controllerse added

---
+++ Country App/Scenes/Home/View/HomeController.swift
@@ -0,0 +1,31 @@
+//
+//  HomeController.swift
+//  Country App
+//
+//  Created by Vasif Mehdi on 21.11.22.
+//
+
+import UIKit
+
+class HomeController: UIViewController {
+
+    @IBOutlet weak var searchTextField: UITextField!
+    @IBOutlet weak var tabelView: UITableView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
+
+//extension HomeController : UITableViewDelegate, UITableViewDataSource {
+//    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+//        <#code#>
+//    }
+//    
+//    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+//        <#code#>
+//    }
+//    
+//    
+//}
