aea72d43c6ea8bb7d3e083c53e573c38e12f69c0
VasifMehdizade <109298511+VasifMehdizade@users.noreply.github.com>
2022-11-21 20:58:09 +0400

    HomeViewModel added

---
+++ Country App/Scenes/Home/ViewModel/HomeViewModel.swift
@@ -0,0 +1,29 @@
+//
+//  HomeViewModel.swift
+//  Country App
+//
+//  Created by Vasif Mehdi on 21.11.22.
+//
+
+import Foundation
+
+class HomeViewModel {
+    
+    // MARK: Varibles
+
+//    var searchResults = [SearchResult]()
+//
+//    var successCallback : (()->())?
+//    var errorCallback : ((String)->())?
+//    
+//    func getSearchResults (text : String) {
+//        SearchManager.shared.searchResults(text: text) { items, errorMessage in
+//            if let errorMessage = errorMessage {
+//                self.errorCallback?(errorMessage)
+//            } else if let docs = items?.results {
+//                self.searchResults = docs
+//                self.successCallback?()
+//            }
+//        }
+//    }
+}
