5ca7689d52d38dce656323212cdeba0d8491f06d
VasifMehdizade <109298511+VasifMehdizade@users.noreply.github.com>
2022-12-18 17:44:38 +0400

    Some constraints added to labels in Details Controller

--- Country App/Scenes/Home/View/Controller/Detail/DetailController.swift
+++ Country App/Scenes/Home/View/Controller/Detail/DetailController.swift
@@ -8,11 +8,31 @@
 import UIKit
 
 class DetailController: UIViewController {
-
+    
+    
+    @IBOutlet weak var navLabel: UILabel!
+    @IBOutlet weak var countryName: UILabel!
+    @IBOutlet weak var regionLabel: UILabel!
+    @IBOutlet weak var regionResponse: UILabel!
+    @IBOutlet weak var areaLabel: UILabel!
+    @IBOutlet weak var areaResponse: UILabel!
+    @IBOutlet weak var populationLabel: UILabel!
+    @IBOutlet weak var populationResponse: UILabel!
+    @IBOutlet weak var currencyLabel: UILabel!
+    @IBOutlet weak var currencyResponse: UILabel!
+    @IBOutlet weak var flagImage: UIImageView!
+    @IBOutlet weak var capitalLabel: UILabel!
+    @IBOutlet weak var capitalResponse: UILabel!
+    @IBOutlet weak var timezoneLabel: UILabel!
+    @IBOutlet weak var timezoneResponse: UILabel!
+    @IBOutlet weak var populationArea: UILabel!
+    @IBOutlet weak var populationAreaResponse: UILabel!
+    @IBOutlet weak var languageLabel: UILabel!
+    @IBOutlet weak var languageResponse: UILabel!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
     }
-
-
 }
